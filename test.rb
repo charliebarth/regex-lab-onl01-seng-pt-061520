ff2c038de882645745f93ed6a60d8b17ede0b362
Charlie Barth <charliepbarth@gmail.com>
2020-06-30 18:35:18 +0000

    Automatically backed up by Learn

--- test.rb
+++ test.rb
@@ -1,29 +1,10 @@
 
  
-def first_word_capitalized_and_ends_with_punctuation?(text)
-one = text.split
-array = []
-count = one.size - 1
-
-one.each do |word|
-  if one[0] == word
-    if word[0] == word[0].upcase
-     array << 1
-    else
-      array << 2
-    end
-  elsif one[count] == word
-    if word.end_with?(".", "!", "?")
-      array << 1
-    else
-      array << 2
+count = 0
+  phone.each do |num|
+    num.size.times do
+      puts num[count]
+      count += 1
     end
   end
- end
- array.all? do |num|
-    num.odd?
-  end
-  puts array
-end
-
-first_word_capitalized_and_ends_with_punctuation?("Extreme briny crepe parking snaps grouping snafu round dog be fork spoon")
+  
